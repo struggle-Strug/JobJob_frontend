d65915046775023356feecf5bc0a562584dff115
struggle-Strug <strugstruggle0430@gmail.com>
2025-04-15 20:10:34 -0700

    fix rule page

--- src/Pages/Rule/index.jsx
+++ src/Pages/Rule/index.jsx
@@ -8,7 +8,7 @@ const CSRule = () => {
   }, []);
   return (
     <>
-      <div className="pt-16 pb-8 bg-[#EFEFEF] h-full">
+      <div className="pt-4 pb-8 bg-[#EFEFEF] h-full">
         <div className="max-w-[1000px] mx-auto bg-white shadow-lg">
           <div className="p-6 border-b-[1px] border-[#EFEFEF]">
             <h1 className="lg:text-xl md:text-lg text-base font-bold text-[#343434]">
